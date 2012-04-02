031b44849ba013ebcfa8e59567ae3991cef02828
Synergy Dev <synergycomputing@gmail.com>
2012-04-02 10:54:26 -0700

    update

--- holiday.mk
+++ holiday.mk
@@ -144,6 +144,10 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/holiday/prebuilt/system/etc/media_profiles.xml:system/etc/media_profiles.xml
+
 PRODUCT_PACKAGES += \
     librs_jni \
     copybit.msm8660 \
