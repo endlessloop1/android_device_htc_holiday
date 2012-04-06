b7432539098cc6c4b869c5b41b769de769ea571e
Synergy Dev <synergycomputing@gmail.com>
2012-04-05 22:14:11 -0700

    Revert "wifi commit"
    This reverts commit d582cbb8729c6ded7f222c462dd8ea0e77e362f8.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcm4330.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcm4330_apsta.bin"
 WIFI_DRIVER_MODULE_NAME     := "bcmdhd"
-WIFI_DRIVER_MODULE_ARG      := "iface_name=wlan0 firmware_path=/system/vendor/firmware/fw_bcm4330.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_ARG      := "iface_name=eth0 firmware_path=/system/vendor/firmware/fw_bcm4330.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 
 # Audio 
 #BOARD_USES_GENERIC_AUDIO := false
