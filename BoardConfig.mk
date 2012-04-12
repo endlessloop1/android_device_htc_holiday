1b23484820a04d383778e5457ee6dc4c8d06ce32
Synergy Dev <synergycomputing@gmail.com>
2012-04-12 12:27:32 -0700

    wlan0 switchup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_MODULE_NAME     := "bcmdhd"
-WIFI_DRIVER_MODULE_ARG      := "iface_name=eth0 firmware_path=/system/vendor/firmware/fw_bcmdhd.bin nvram_path=/proc/calibration"
+WIFI_DRIVER_MODULE_ARG      := "iface_name=wlan0 firmware_path=/system/vendor/firmware/fw_bcmdhd.bin nvram_path=/proc/calibration"
 
 # Audio 
 #BOARD_USES_GENERIC_AUDIO := false
