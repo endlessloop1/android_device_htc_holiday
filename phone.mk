c6c20cd4fa42c55937936b1aafe935714c1c912f
Synergy Dev <synergycomputing@gmail.com>
2012-04-21 20:34:54 -0700

    new makefile cleanup unnecessary prop lines

--- phone.mk
+++ phone.mk
@@ -13,6 +13,3 @@ PRODUCT_NAME := htc_holiday
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := HTC Vivid
 PRODUCT_MANUFACTURER := HTC
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_holiday ro.build.fingerprint=telstra_wwe/htc_holiday/holiday:4.0.3/IML74K/300623.4:user/release-keys PRIVATE_BUILD_DESC="3.25.841.4 CL300623 release-keys" BUILD_NUMBER=300623
